7aef909b55cf5ede30e7e83bfdcc8f12c5559dd2
marneneha <nehamarne27@gmail.com>
2023-04-19 21:05:16 -0400

    able to load env as per 2d env and turtlebot with lidar

---
+++ src/gazebo_node.py
@@ -0,0 +1,18 @@
+import rospy
+import time
+
+# step function
+def step():
+    # update the state using ros subscriber to odom
+    # if numer_of_iteration = ep_duration is over reset the environment
+    
+    pass
+# reset function
+def reset():
+    # set the startpose of the robot 
+    # set the goal pose here 
+    # set the velocity to zero
+    # change the obstacle if you want
+    pass
+# render function
+# if __name__ == "main":
